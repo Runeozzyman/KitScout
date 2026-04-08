5ea8ddc07b5a8cffdf5462feefbb08ed7eda5354
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-04-07 19:40:10 -0600

    Update header.tsx

--- kitscout/components/header.tsx
+++ kitscout/components/header.tsx
@@ -4,17 +4,18 @@ export default function Header(){
 
     const router = useRouter();
 
-    return(
+return(
+
     <div className="w-full border-b bg-white sticky top-0 z-50 shadow-sm">
         <div className="max-w-5xl mx-auto flex items-center justify-center p-6">
             <button
-            onClick={() => router.push("/")}
-            className="text-lg sm:text-xl font-bold tracking-tight hover:cursor-pointer"
+                onClick={() => router.push("/")}
+                className="text-lg sm:text-xl font-bold tracking-tight hover:cursor-pointer"
             >
             KitScout
             </button>
         </div>
-    </div>
+    </div>  
 
     )
 }
